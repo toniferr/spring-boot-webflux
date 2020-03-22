f33b15116df55f478697ad436f93b48ec493ce4c
toni <ferreirocouto@gmail.com>
2020-03-22 19:36:20 +0100

    Drop collecion

--- src/main/java/com/toni/springboot/webflux/app/SpringBootWebfluxApplication.java
+++ src/main/java/com/toni/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
 import com.toni.springboot.webflux.app.models.dao.ProductoDao;
 import com.toni.springboot.webflux.app.models.documents.Producto;
@@ -18,6 +19,9 @@ public class SpringBootWebfluxApplication implements CommandLineRunner{
 	@Autowired
 	private ProductoDao dao;
 	
+	@Autowired
+	private ReactiveMongoTemplate mongoTemplate;
+	
 	private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
 
 	public static void main(String[] args) {
@@ -26,6 +30,7 @@ public class SpringBootWebfluxApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		mongoTemplate.dropCollection("productos").subscribe();
 		Flux.just(
 				new Producto("Panasonic TV", 200.20),
 				new Producto("Sony TV", 300.39),
