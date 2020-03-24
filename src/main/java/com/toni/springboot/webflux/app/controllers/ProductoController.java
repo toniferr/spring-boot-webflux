13c981377cd29840949dac2bfe1ee7fb9c5c1864
toni <ferreirocouto@gmail.com>
2020-03-24 13:26:29 +0100

    Reactive Data Driver

--- src/main/java/com/toni/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/toni/springboot/webflux/app/controllers/ProductoController.java
@@ -1,11 +1,14 @@
 package com.toni.springboot.webflux.app.controllers;
 
+import java.time.Duration;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 
 import com.toni.springboot.webflux.app.models.dao.ProductoDao;
 import com.toni.springboot.webflux.app.models.documents.Producto;
@@ -31,6 +34,21 @@ public class ProductoController {
 		productos.subscribe(prod -> log.info(prod.getNombre()));
 		
 		model.addAttribute("productos",productos);
+		model.addAttribute("titulo","Listado de productos");
+		return "listar";
+	}
+	
+	@GetMapping("/listar-datadriver")
+	public String listarDataDriver (Model model) {
+		
+		Flux<Producto> productos = dao.findAll().map(producto -> {
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		}).delayElements(Duration.ofSeconds(1));
+		
+		productos.subscribe(prod -> log.info(prod.getNombre()));
+		
+		model.addAttribute("productos",new ReactiveDataDriverContextVariable(productos, 1));
 		model.addAttribute("titulo","Listado de productos");
 		return "listar";
 	}
